ef1e96681dc919436d360240250a52e49a8ec994
Eduardo <juan.gomez@cceo.com.mx>
2021-11-04 17:29:54 -0600

    Agregamos componente Agregar Tarea

--- Lista de Tareas/vue/codevue.js
+++ Lista de Tareas/vue/codevue.js
@@ -1,3 +1,21 @@
+var data = {
+    tareas: [
+        {
+            texto: 'Aprender Vue.js',
+            terminada: false
+        },
+        {
+            texto: 'Aprender Angular 2',
+            terminada: false
+        },
+        {
+            texto: 'Aprender Ionic 2',
+            terminada: false
+        }
+    ],
+    nuevaTarea: ''
+}
+
 Vue.component('titulo', {
     template: '<h2>{{titulo}}</h2>',
     data: function(){
@@ -5,36 +23,41 @@ Vue.component('titulo', {
     }
 })
 
+Vue.component('nueva-tarea', {
+    template: `<div class="input-group">
+    <input type="text"
+        placeholder="Escribe Una Nueva Tarea" 
+        v-model="nuevaTarea"
+        class="form-control"
+        @keyup.enter="agregarTarea">
+    <span class="input-group-btn">
+        <button type="button" 
+        @click="agregarTarea" 
+        class="btn btn-primary">Agregar Tarea</button>
+    </span>
+</div> `,
+
+data: function() {
+    return data;
+},
+methods: {
+    agregarTarea: function() {
+        var texto = this.nuevaTarea.trim();
+        if (texto) {
+            this.tareas.push ({
+                texto: texto,
+                terminada: false
+            })
+        }
+        this.nuevaTarea ='';
+    },
+}
+})
+
 var app = new Vue ({
     el: '#app',
-    data: {
-        tareas: [
-            {
-                texto: 'Aprender Vue.js',
-                terminada: false
-            },
-            {
-                texto: 'Aprender Angular 2',
-                terminada: false
-            },
-            {
-                texto: 'Aprender Ionic 2',
-                terminada: false
-            }
-        ],
-        nuevaTarea: ''
-    },
+    data:data,
     methods: {
-        agregarTarea: function() {
-            var texto = this.nuevaTarea.trim();
-            if (texto) {
-                this.tareas.push ({
-                    texto: texto,
-                    terminada: false
-                })
-            }
-            this.nuevaTarea ='';
-        },
         borrar: function (indice){
             this.tareas.splice(indice, 1);
         }
