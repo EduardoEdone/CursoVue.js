0263d354889aa324749d3bfe5b160e5f2216894b
Eduardo <juan.gomez@cceo.com.mx>
2021-11-05 16:23:07 -0600

    Se agrega proyecto myapp de vue CLI

---
+++ myapp/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class="container">
+    <div class="jumbotron">
+      <titulo :titulo="titulo"></titulo>
+    </div>
+  </div>
+</template>
+
+<script>
+import Titulo from './TituloComponent.vue'
+export default {
+  components: {
+    Titulo
+  },
+  data() {
+    return {
+      titulo: '- Lista De Tareas -'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
