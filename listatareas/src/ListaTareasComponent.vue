931433b358f817b282a47d953d94381aec0f5476
Eduardo <juan.gomez@cceo.com.mx>
2021-11-05 18:01:26 -0600

    Creamos componentes e importamos

---
+++ listatareas/src/ListaTareasComponent.vue
@@ -0,0 +1,29 @@
+<template>
+   <ul class="list-group">
+    <li v-for="(tarea, indice) of tareas" class="list-group-item"
+    :class="{terminada: tarea.terminada}">
+        {{tarea.texto}}
+        <span class="pull-right">
+            <button type="button" class="btn btn-success btn-xs glyphicon glyphicon-ok"
+            @click="tarea.terminada = !tarea.terminada">
+            </button>
+            <button type="button" class="btn btn-danger btn-xs glyphicon glyphicon-remove"
+            @click="borrar(indice)">
+            </button>
+        </span>
+    </li>
+</ul>
+</template>
+
+<script>
+export default {
+    props: ['tareas']
+}
+</script>
+
+<style>
+    .terminada {
+    color: gray;
+    text-decoration: line-through;
+}
+</style>
---
+++ listatareas/src/NuevaTareaComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="input-group">
+    <input type="text"
+        placeholder="Escribe Una Nueva Tarea" 
+        v-model="nuevaTarea"
+        class="form-control"
+        @keyup.enter="agregarTarea">
+    <span class="input-group-btn">
+        <button type="button" 
+        @click="agregarTarea" 
+        class="btn btn-primary">Agregar Tarea</button>
+    </span>
+</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            nuevaTarea: ''
+        }
+    },
+    props: ['tareas'],
+    methods: {
+        agregarTarea(){
+            var texto = this.nuevaTarea.trim();
+            if (texto){
+                this.tareas.push ({
+                    texto: texto,
+                    terminada: false
+                })
+            }
+            this.nuevaTarea='';
+        }
+    }
+}
+</script>
