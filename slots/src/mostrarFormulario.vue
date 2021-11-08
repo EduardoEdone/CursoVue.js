813c36b3554cf13e2b18a274544a33a1a0ab0569
Eduardo <juan.gomez@cceo.com.mx>
2021-11-08 17:59:35 -0600

    Agregamos componente mostrarFormulario

---
+++ slots/src/mostrarFormulario.vue
@@ -0,0 +1,25 @@
+<template>
+    <form class="form">
+        <h3>Iniciar Sesión</h3>
+        <div class="form-group">
+            <label for="correo">Correo Electrónico</label>
+            <input type="email" class="form-control"
+                    id="correo" placeholder="Escribe Tu Correo Electrónico">
+        </div>
+    <div class="form-group">
+        <label for="clave">Contraseña</label>
+            <input type="password" class="form-control"
+                    id="clave" placeholder="Escribe Tu Contraseña">
+        </div>
+        <button class="btn btn-primary">Iniciar Sesión</button>
+    </form>
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+
+</style>
