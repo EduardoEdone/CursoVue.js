89e780bc4b36d04a98b40d41d11069ae4d55374e
Eduardo <juan.gomez@cceo.com.mx>
2021-11-09 16:08:46 -0600

    Termina proyecto Directivas

---
+++ dirper/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <span v-decorar:grande.italico.negrilla="{familia: 'helevetica', color: 'red'}">
+        Aprende Vue.js 2 Facilmente</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
